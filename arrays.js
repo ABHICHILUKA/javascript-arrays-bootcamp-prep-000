2464e42dd371fccdf81743578ccf7bac37c7b0b0
Nikhil Chiluka <chnikhil058@gmail.com>
2018-02-22 06:59:44 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,38 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
+function addElementToBeginningOfArray (array,element){
+  var newarray=new Array();
+   newarray = array
+   return newarray.unshift(element)
+}
+function destructivelyAddElementToBeginningOfArray(array,element){
+  return array.unshift("$element")
+}
+function addElementToEndOfArray(array,element){
+  var newarray=new Array();
+  newarray = array
+  return newarray.push('$element')
+}
+function destructivelyAddElementToEndOfArray(array,element){
+  return array.push('$element')
+}
+function accessElementInArray(array,index){
+  return array[index]
+}
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array.shift()
+}
+function removeElementFromBeginningOfArray(array){
+  var sliceele=new Array();
+  sliceele=array.slice(0,0)
+  return array
+}
+function destructivelyRemoveElementFromEndOfArray (array){
+  array.pop()
+ return array
+}
+function removeElementFromEndOfArray(array){
+  var newarray =new Array();
+  newarray= array
+  newarray.pop(-1)
+  return newarray
+}
